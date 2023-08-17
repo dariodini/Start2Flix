666958f7ed5a733964de9f4e134fa8a5e3e74a2f
Dario Di Nicuolo <dado02rio02@gmail.com>
2023-08-17 17:35:43 +0200

    Creata rotta per navigare il catalogo, creata lista statica film

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 
-import SignupView from '../views/SignupView.vue';
-import LoginView from '../views/LoginView.vue';
+import SignupView from '../views/SignupView.vue'
+import LoginView from '../views/LoginView.vue'
 import LandingView from '../views/LandingView.vue'
 import SelectProfile from '../views/SelectProfile.vue'
+import BrowseCatalogue from '../views/BrowseCatalogue.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: LandingView, meta: {
-      showLoginButton: true
-    } },
-    { path: '/login', component: LoginView, meta: {
-      showLoginButton: true
-    } },
-    { path: '/signup', component: SignupView, meta: {
-      showLoginButton: true
-    } },
-    { path: '/select-profile', component: SelectProfile, meta: {
-      hideHeader: true
-    } },
+    {
+      path: '/',
+      component: LandingView,
+      meta: {
+        showLoginButton: true
+      }
+    },
+    {
+      path: '/login',
+      component: LoginView,
+      meta: {
+        showLoginButton: true
+      }
+    },
+    {
+      path: '/signup',
+      component: SignupView,
+      meta: {
+        showLoginButton: true
+      }
+    },
+    {
+      path: '/select-profile',
+      component: SelectProfile,
+      meta: {
+        hideHeader: true
+      }
+    },
+    {
+      path: '/browse',
+      component: BrowseCatalogue
+    }
   ]
 })
 
---
+++ src/views/BrowseCatalogue.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="row">
+    <div
+      v-for="product in products"
+      :key="product"
+      class="col col-lg-2 col-md-4 col-sm-6 col-12 gy-5"
+    >
+      <product-card :product="product"></product-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductCard from '../components/ProductCard.vue'
+
+export default {
+  components: {
+    ProductCard
+  },
+  data() {
+    return {
+      products: [
+        {
+          title: 'Stranger Things',
+          imageUrl: 'src/assets/stranger-things.jpeg'
+        },
+        {
+          title: 'Stranger Things',
+          imageUrl: 'src/assets/stranger-things.jpeg'
+        },
+        {
+          title: 'Stranger Things',
+          imageUrl: 'src/assets/stranger-things.jpeg'
+        },
+        {
+          title: 'Stranger Things',
+          imageUrl: 'src/assets/stranger-things.jpeg'
+        }
+      ]
+    }
+  }
+}
+</script>
