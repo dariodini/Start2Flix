621c5ba8afc3f5591d9a801b566d5498222aa3ef
dariodini <dado02rio02@gmail.com>
2023-08-28 15:27:28 +0200

    Sistemata ProductCard

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="product-card">
-    <div class="product-card__image-container">
-      <img :src="product.imageUrl" :alt="product.title" class="product-card__image" />
+    <div class="product-card__image-container" @click="openModal">
+      <img
+        :src="`https://image.tmdb.org/t/p/original` + product.poster_path"
+        :alt="`Poster of '` + product.title + `'`"
+        class="product-card__image"
+      />
     </div>
+
     <div class="product-card__details">
       <div class="product-card__title">{{ product.title }}</div>
       <div class="product-card__actions">
-        <button class="product-card__button play-button">
-          <img src="../assets/play-button.svg" alt="Play button" />
-        </button>
-        <button class="product-card__button add-to-list-button">
-          <img src="../assets/add-to-list.svg" alt="Add to list button" />
-        </button>
+        <div class="button-group">
+          <button class="product-card__button play-button">
+            <img src="../assets/play-button.svg" alt="Play button" />
+          </button>
+          <button class="product-card__button add-to-list-button">
+            <img src="../assets/add-to-list.svg" alt="Add to list button" />
+          </button>
+        </div>
         <button class="product-card__button expand-button">
-          <img src="../assets/expand-button.svg" alt="Expand button" />
+          <img src="../assets/expand-button.svg" alt="Expand button" @click="openModal" />
         </button>
       </div>
     </div>
@@ -22,8 +29,13 @@
 
 <script>
 export default {
-  props: ["product"],
-};
+  props: ['product'],
+  methods: {
+    openModal() {
+      this.$emit('open-modal', this.product)
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -37,11 +49,21 @@ export default {
 
   &:hover {
     box-shadow: rgba(0, 0, 0, 0.75) 0px 3px 10px;
-    transition: 0.4s all ease-in-out;
-    transform: scale(1.1);
+    transition: 0.3s all ease-in-out;
+    transform: scale(1.03);
     pointer-events: auto;
+
     #{$self}__details {
       opacity: 1;
+
+      @media screen and (max-width: 576px) {
+        opacity: 0;
+      }
+    }
+
+    @media screen and (max-width: 576px) {
+      transform: initial;
+      box-shadow: none;
     }
   }
 
@@ -54,8 +76,9 @@ export default {
   }
   &__details {
     opacity: 0;
-    padding: 16px;
+    padding: 14px;
     background-color: #141414;
+    transition: 0.3s all ease-in-out;
   }
   &__title {
     color: white;
@@ -65,7 +88,7 @@ export default {
 
   &__actions {
     display: flex;
-    justify-content: start;
+    justify-content: space-between;
     align-items: center;
     flex-direction: row;
   }
@@ -73,8 +96,8 @@ export default {
   &__button {
     border: 2px solid grey;
     background-color: transparent;
-    width: 42px;
-    height: 42px;
+    width: 35px;
+    height: 35px;
     margin: 4px;
     border-radius: 50%;
     display: flex;
@@ -84,35 +107,33 @@ export default {
     &:hover {
       border: 2px solid rgb(229, 229, 229);
     }
+
     img {
-      width: 25px;
-      height: 25px;
+      width: 17px;
+      height: 17px;
     }
   }
 }
+
 .play-button {
   border: 0;
   background-color: white;
   padding-left: 10px;
+
+  @media screen and (min-width: 1050px) {
+    padding-left: 6px;
+  }
+
+  @media screen and (min-width: 900px) {
+    padding-left: 8px;
+  }
 
   &:hover {
     border: 0;
   }
 }
 
-.add-to-list-button {
-  img {
-    width: 22px;
-    height: 22px;
-  }
-}
 .expand-button {
-  margin-left: 30px;
-  padding-top: 6px;
-
-  img {
-    width: 20px;
-    height: 11px;
-  }
+  padding-top: 5px;
 }
 </style>
