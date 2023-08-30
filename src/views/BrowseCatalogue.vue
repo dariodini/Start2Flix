aa3cee5a8b2d9cece96bdb52d9702517983f75ce
dariodini <dado02rio02@gmail.com>
2023-08-30 15:08:32 +0200

    Assemblata pagina BrowseCatalogue

--- src/views/BrowseCatalogue.vue
+++ src/views/BrowseCatalogue.vue
@@ -1,36 +1,55 @@
 <template>
-  <div class="products-container" v-if="!isMoviesLoading">
-    <div class="section movies" v-if="movies.results.length > 0">
-      <div class="section__title"><span>Movies</span></div>
-      <div class="row">
-        <div
-          v-for="product in movies.results"
-          :key="product.id"
-          class="product col-xl-2 col-lg-3 col-md-4 col-sm-6 col-4"
-        >
-          <product-card :product="product" @open-modal="openModal"></product-card>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div v-else>Caricamento in corso...</div>
+  <product-section
+    :isProductsLoading="isMoviesLoading"
+    :products="movies"
+    title="Movies"
+    @open-modal="openModal"
+  ></product-section>
+
+  <product-detail-modal
+    :product="selectedProduct"
+    v-if="showModal"
+    @close-modal="closeModal"
+  ></product-detail-modal>
 </template>
 
 <script>
-import ProductCard from '../components/productcard.vue'
+import ProductSection from '../components/ProductSection.vue'
+import ProductDetailModal from '../components/ProductDetailModal.vue'
 import { mapGetters } from 'vuex'
 
 export default {
   components: {
-    ProductCard,
+    ProductDetailModal,
+    ProductSection
   },
   data() {
     return {
+      showModal: false,
+      selectedProduct: null
+    }
+  },
   async beforeMount() {
     await this.$store.dispatch('fetchMovies')
   },
   computed: {
     ...mapGetters(['movies', 'isMoviesLoading'])
   },
-};
+  created() {
+    this.emitter.on('open-modal', this.handleOpenModal)
+  },
+  methods: {
+    openModal(product) {
+      this.showModal = true
+      this.selectedProduct = product
+    },
+    closeModal() {
+      this.showModal = false
+    },
+    handleOpenModal(product) {
+      this.selectedProduct = product
+      this.showModal = true
+    }
+  }
+}
 </script>
