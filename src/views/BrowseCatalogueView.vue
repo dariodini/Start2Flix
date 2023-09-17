1a10af9fd83b9e3ba237156b3e1c77ed140db372
Dario Di Nicuolo <dado02rio02@gmail.com>
2023-09-17 22:52:41 +0200

    Aggiunta sezione per visualizzare prodotti ricercati

--- src/views/BrowseCatalogueView.vue
+++ src/views/BrowseCatalogueView.vue
@@ -1,17 +1,26 @@
 <template>
-  <product-section
-    :isProductsLoading="isMoviesLoading"
-    :products="movies"
-    title="Movies"
-    @open-modal="openModal"
-  ></product-section>
+  <div v-if="!isFiltered">
+    <product-section
+      :isProductsLoading="isMoviesLoading"
+      :products="movies"
+      title="Movies"
+    ></product-section>
 
-  <product-section
-    :isProductsLoading="isSeriesLoading"
-    :products="series"
-    title="Series"
-    @open-modal="openModal"
-  ></product-section>
+    <product-section
+      :isProductsLoading="isSeriesLoading"
+      :products="series"
+      title="Series"
+    ></product-section>
+  </div>
+
+  <div v-else>
+    <product-section
+      :notCarousel="true"
+      :isProductsLoading="isFilteredProductsLoading"
+      :products="filteredProducts"
+      title="Titoli che potrebbero corrispondere con quello che stai cercando:"
+    ></product-section>
+  </div>
 
   <product-detail-modal
     :product="selectedProduct"
@@ -21,43 +30,58 @@
 </template>
 
 <script>
-import ProductSection from "../components/ProductSection.vue";
-import ProductDetailModal from "../components/ProductDetailModal.vue";
-import { mapGetters } from "vuex";
+import ProductSection from '../components/ProductSection.vue'
+import ProductDetailModal from '../components/ProductDetailModal.vue'
+import { mapGetters } from 'vuex'
 
 export default {
   components: {
     ProductDetailModal,
-    ProductSection,
+    ProductSection
   },
   data() {
     return {
       showModal: false,
       selectedProduct: null,
-    };
+      isFiltered: false
+    }
   },
   async beforeMount() {
-    await this.$store.dispatch("fetchMovies");
-    await this.$store.dispatch("fetchSeries");
+    await this.$store.dispatch('fetchMovies')
+    await this.$store.dispatch('fetchSeries')
   },
   computed: {
-    ...mapGetters(["movies", "isMoviesLoading", "series", "isSeriesLoading"]),
+    ...mapGetters([
+      'movies',
+      'isMoviesLoading',
+      'series',
+      'isSeriesLoading',
+      'filteredProducts',
+      'isFilteredProductsLoading'
+    ])
   },
   created() {
-    this.emitter.on("open-modal", this.handleOpenModal);
+    this.emitter.on('open-modal', this.handleOpenModal)
+    this.emitter.on('filter-products', this.filterResults)
   },
   methods: {
     openModal(product) {
-      this.showModal = true;
-      this.selectedProduct = product;
+      this.showModal = true
+      this.selectedProduct = product
     },
     closeModal() {
-      this.showModal = false;
+      this.showModal = false
     },
     handleOpenModal(product) {
-      this.selectedProduct = product;
-      this.showModal = true;
+      this.selectedProduct = product
+      this.showModal = true
     },
-  },
-};
+    async filterResults(terms) {
+      if (terms.length > 0) {
+        this.isFiltered = true
+        await this.$store.dispatch('filterProducts', terms)
+      } else this.isFiltered = false
+    }
+  }
+}
 </script>
