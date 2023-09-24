31aa188b7bc511628a6842dfd5df46ae526b38fa
Dario Di Nicuolo <dado02rio02@gmail.com>
2023-09-24 15:10:53 +0200

    Aggiunte Routes (esposti gli endpoint per le api) e Router

---
+++ start2flix_backend/App/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+$router->post('api/utente', 'ApiUtenteController@addNewUtente');
+$router->put('api/utente', 'ApiUtenteController@updateUtente');
+$router->delete('api/utente', 'ApiUtenteController@deleteUtente');
+$router->get('api/utente/profili', 'ApiUtenteController@getProfiles');
+
+
+$router->post('api/utente/profilo', 'ApiProfiloController@addNewProfile');
+$router->put('api/utente/profilo', 'ApiProfiloController@updateProfile');
+$router->delete('api/utente/profilo', 'ApiProfiloController@deleteProfile');
+$router->get('api/profilo/get-products', 'ApiProfiloController@getProducts');
+
+
+$router->post('api/profilo/add-product', 'ApiProfiloProdottoController@addProductToProfileList');
+$router->delete('api/profilo/remove-product', 'ApiProfiloProdottoController@deleteProductToProfileList');
