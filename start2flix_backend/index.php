fc9465208b865318f076382025f5bef413992e38
Dario Di Nicuolo <dado02rio02@gmail.com>
2023-09-24 22:19:33 +0200

    Aggiunti Header per CORS

--- start2flix_backend/index.php
+++ start2flix_backend/index.php
@@ -5,5 +5,8 @@ require 'core/bootstrap.php';
 
 use App\Core\{Router, Request};
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+
 Router::load('App/Routes.php')
   ->direct(Request::uri(), Request::method());
