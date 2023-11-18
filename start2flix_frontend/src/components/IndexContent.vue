677ab63466b488e919c6df0827c26d13ab44da80
Dario Di Nicuolo <dariodinicuolo@gmail.com>
2023-11-18 20:04:45 +0100

    Creato componente IndexContent per manage-account

---
+++ start2flix_frontend/src/components/IndexContent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="index-content row">
+    <h2 class="index-content__index col-md-3">{{ index }}</h2>
+    <div class="index-content__content col-md-9">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['index']
+}
+</script>
+
+<style lang="scss" scoped>
+.index-content {
+  border-top: 1px solid #999;
+  padding: 1rem 0;
+
+  &__index {
+    font-size: 1.25rem;
+    font-weight: 300;
+    text-transform: uppercase;
+
+    color: #737373;
+  }
+
+  &__content {
+    padding-left: 2rem;
+  }
+}
+</style>
